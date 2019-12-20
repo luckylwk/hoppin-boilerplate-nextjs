45963c2ac8b2246c2693f1122d8f423b7c6bdc05
Mike Wong <wongmdesign@gmail.com>
2019-12-20 11:04:42 -0500

    completed hooks test

--- pages/form/index.jsx
+++ pages/form/index.jsx
@@ -1,12 +1,27 @@
 // import { Component } from "react";
 // import PropTypes from "prop-types";
-import { withRouter } from 'next/router';
+import { withRouter } from 'next/router'
 
-import { Container, Box } from 'hoppin-design-system';
+import { Container, Box } from 'hoppin-design-system'
+
+import React, { useState } from 'react'
 
 const FormPage = ({ router }) => {
+  const [username, setUsername] = useState('')
+  const [checkbox, setCheckbox] = useState(false)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setUsername(value)
+  }
+
+  const handleCheckboxChange = (e) => {
+    const { name, checked } = e.target
+    setCheckbox(checked)
+  }
+
   return (
-    <Container marginY="base" width="narrow">
+    <Container marginY='base' width='narrow'>
       <div>
         Instructions:
         <ol>
@@ -14,16 +29,39 @@ const FormPage = ({ router }) => {
             1a. Add a form with controlled inputs. Use one text field and one
             checkbox.
           </li>
-          <li>1b. Implement or update to use hooks. If not explain what they may improve.</li>
-          <li>2a. Implement or Pseudo-code an async select input that requests a company name</li>
-          <li>2b. URL when needed: https://autocomplete.clearbit.com/v1/companies/suggest?query=hoppin</li>
+          <li>
+            1b. Implement or update to use hooks. If not explain what they may
+            improve.
+          </li>
+          <li>
+            2a. Implement or Pseudo-code an async select input that requests a
+            company name
+          </li>
+          <li>
+            2b. URL when needed:
+            https://autocomplete.clearbit.com/v1/companies/suggest?query=hoppin
+          </li>
         </ol>
       </div>
-      <Box padding="base" bg="neutral.lighter">
-        <div>Insert form here.</div>
+      <Box padding='base' bg='neutral.lighter'>
+        <div>
+          <input
+            name='username'
+            type='text'
+            onChange={handleChange}
+            value={username}
+          />
+          ;
+          <input
+            type='checkbox'
+            onChange={handleCheckboxChange}
+            name='checkbox'
+            checked={checkbox}
+          />
+        </div>
       </Box>
     </Container>
-  );
-};
+  )
+}
 
-export default withRouter(FormPage);
+export default FormPage
